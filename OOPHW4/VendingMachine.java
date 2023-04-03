ecf61ea659b5cfbcc326cffdc386efbd7719f9a7
Borovkov A <kekushf@gmail.com>
2023-04-03 19:53:37 +0300

    HW4

---
+++ OOPHW4/VendingMachine.java
@@ -0,0 +1,18 @@
+package OOPHW4;
+
+/**
+ * Интерфейс торгового автомата
+ */
+public interface VendingMachine {
+    /**
+     * Внесение продуктов в торговый автомат
+     */
+    void initProduct();
+
+    /**
+     * Получение продукта из торгового автомата
+     *
+     * @return строку
+     */
+    String getProduct();
+}
